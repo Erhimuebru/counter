6ae3d7f05e4a314779742406760ea96ff11fcd6c
Erhimuebru <erhimu.ebru@decagon.dev>
2022-10-21 03:25:29 +0100

    counter

---
+++ app.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+let btns = document.querySelectorAll('.btn');
+let value = document.getElementById('value');
+
+btns.forEach((btns)=>{
+    btns.addEventListener('click',(e)=>{
+        let styles = e.currentTarget.classList
+        if(styles.contains('decrease')){
+            count--
+        }
+
+         else if(styles.contains('increase')){
+            count++
+        }
+
+        else(count = 0)
+
+        if (count > 0){
+            value.style.color = 'green'
+        }
+
+        if (count < 0){
+            value.style.color = 'red'
+        }
+
+        if (count === 0){
+            value.style.color = 'black'
+        }
+
+        value.textContent = count;
+    })
+})
